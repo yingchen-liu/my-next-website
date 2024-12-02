a1f4fc63ef9f8af5ae8ce62c37798903454dc023
Yingchen <info@yingchenliu.com>
2024-12-02 11:44:40 +0800

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import HeaderMenu from "./components/Common/HeaderMenu";
 
 export const metadata: Metadata = {
   title: "Yingchen Liu | Senior Full-Stack Engineer",
-  description: "Versatile Senior Software Engineer with over 8 years of hands-on experience, specialising in full-stack web, mobile, and cloud technologies.",
+  description:
+    "Versatile Senior Software Engineer with over 8 years of hands-on experience, specialising in full-stack web, mobile, and cloud technologies.",
 };
 
 export default function RootLayout({
@@ -14,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VZ85PQC5NS"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-VZ85PQC5NS');
+          `}
+        </script>
+      </head>
       <body className={`bg-gray-800 text-blue-100 font-light antialiased`}>
         <div className="container mx-auto max-w-5xl px-5 md:px-10">
           <HeaderMenu activeItem="home" />
